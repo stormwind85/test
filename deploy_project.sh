694936bd96a78f3baeeff941b7f7f369c9d4227f
abrechet <adam.brechet@gmail.com>
2017-05-12 18:12:44 +0200

    up circle

--- deploy_project.sh
+++ deploy_project.sh
@@ -1,10 +1,3 @@
-user=IoNiStEr
-password=0cHyDZta
-passwordContainer=Helpdesk16
-passwordJenkins=15f07582ebcb4a39b50a4cadbc21e51c
+port=$(wget --no-check-certificate --user=$USER --password=$PASSWORD -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
 
-port=$(wget --no-check-certificate --user=$user --password=$password -qO- https://docker-knock-auth.hipay.org/KyP54YzX/?srvname=deploy.hipay-pos-platform.com)
-
-echo $port
-
-sshpass -p $passwordContainer ssh root@docker-knock-auth.hipay.org -p $port curl --user admin:$passwordJenkins -s "http://jenkins.hipay-pos-platform.com:8080/job/Repo_test/build?token=tokenRepoTest&cause=Une+cause+quelconque"
+sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port curl --user admin:$PASS_JENKINS -s "http://jenkins.hipay-pos-platform.com:8080/job/Repo_test/build?token=${TOKEN}&cause=Une+cause+quelconque"

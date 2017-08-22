c55f2c4774f433aa678183c8d09c5ae10490febd
abrechet <adam.brechet@gmail.com>
2017-08-22 16:13:57 +0200

    Test :Edit shell file for git

--- git-push.sh
+++ git-push.sh
@@ -1,8 +1,17 @@
 defaultPush='n'
+defaultMessage=$(git log | head -5 | grep "Test :")
 git add .
 git status
-read -r -p "Please, write your commit message : " message
-git commit -am "$message"
+if [ "$defaultMessage" != "" ]; then
+	read -p "Please, write your commit message. Default [$(echo $defaultMessage)] " message
+	message=${message:-$defaultMessage}
+	git commit -am "$(echo $message)"
+else
+	while [ "$message" = "" ]; do
+		read -r -p "Please, write your commit message. Default [$(echo $defaultMessage)] " message
+	done
+	git commit -am "Test :$message"
+fi
 read -p "Are you sure to push your commit ? Default : [$defaultPush] " push
 push=${push:-$defaultPush}
 if [ "$push" = "y" ]; then

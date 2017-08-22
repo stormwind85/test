e5158df8d3aa06e1547ef14efd85e96c63eda12d
abrechet <adam.brechet@gmail.com>
2017-08-22 15:31:46 +0200

    add sh file for git commit & execute example circle2.0

--- git-push.sh
+++ git-push.sh
@@ -5,6 +5,6 @@ read -r -p "Please, write your commit message : " message
 git commit -am "$message"
 read -p "Are you sure to push your commit ? Default : [$defaultPush] " push
 push=${push:-$defaultPush}
-if [ '$push' = 'y' ]; then
+if [ "$push" = "y" ]; then
 	git push
 fi
